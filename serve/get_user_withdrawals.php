48afeee0d42dde82e23db9fd37848ef64d416542
amethystcoder <75532154+amethystcoder@users.noreply.github.com>
2024-02-29 20:56:45 +0100

    Create get_user_withdrawals.php

---
+++ serve/get_user_withdrawals.php
@@ -0,0 +1,23 @@
+<?php 
+session_start();
+try{
+$user_id = empty($_SESSION["user_id"]) ? "" : $_SESSION["user_id"] ;
+if(!empty($user_id)){
+$withdrawals = json_decode(file_get_contents("withdrawals.json"),true);
+$user_withdrawals = [];
+for($i =0;$i<count($withdrawals);$i++){
+if($withdrawals[$i]["user_id"] == $user_id){
+$user_withdrawals[count($user_withdrawals)] = $withdrawals[$i];
+}
+}
+echo json_encode(array("code"=> 0, "data"=>$user_withdrawals));
+}
+else{
+echo json_encode(array("code"=> 2, "data"=>[]));
+}
+}
+catch(Throwable $th){
+echo json_encode(array("code"=> 1, "data"=>[]));
+}
+
+?>
