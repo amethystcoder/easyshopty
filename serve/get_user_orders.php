f5ca5e39ef78f869e6e7c02535d1cf8697323165
amethystcoder <75532154+amethystcoder@users.noreply.github.com>
2024-02-29 21:26:00 +0100

    Create get_user_orders.php

---
+++ serve/get_user_orders.php
@@ -0,0 +1,23 @@
+<?php 
+session_start();
+try{
+$user_id = empty($_SESSION["user_id"]) ? "" : $_SESSION["user_id"] ;
+if(!empty($user_id)){
+$orders = json_decode(file_get_contents("orders.json"),true);
+$user_orders = [];
+for($i =0;$i<count($orders);$i++){
+if($orders[$i]["user_id"] == $user_id){
+$user_orders[count($user_orders)] = $orders[$i];
+}
+}
+echo json_encode(array("code"=> 0, "data"=>$user_orders));
+}
+else{
+echo json_encode(array("code"=> 2, "data"=>[]));
+}
+}
+catch(Throwable $th){
+echo json_encode(array("code"=> 1, "data"=>[]));
+}
+
+?>
