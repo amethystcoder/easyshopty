0c1001fe9eb97348f46e3e7fd170e58a4f9931df
amethystcoder <75532154+amethystcoder@users.noreply.github.com>
2024-02-29 20:53:00 +0100

    Update admin_get_all_deposits.php

--- serve/admin_get_all_deposits.php
+++ serve/admin_get_all_deposits.php
@@ -1,4 +1,19 @@
-<?php
+//get all deposits admin
+<?php 
 
+session_start();
+try{
+$admin_id = empty($_SESSION["admin_id"]) ? "" : $_SESSION["admin_id"] ;
+if(!empty($admin_id)){
+$deposits = json_decode(file_get_contents("deposits.json"),true);
+echo json_encode(array("code"=> 0, "data"=>$deposits));
+}
+else{
+echo json_encode(array("code"=> 1, "data"=>[]));
+}
+}
+catch(Throwable $th){
+echo json_encode(array("code"=> 1, "data"=>[]));
+}
 
 ?>
