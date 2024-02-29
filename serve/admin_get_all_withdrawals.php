04cdc3112d585d355d27bab17e7668866b0aa95d
amethystcoder <75532154+amethystcoder@users.noreply.github.com>
2024-02-29 20:55:48 +0100

    Create admin_get_all_withdrawals.php

---
+++ serve/admin_get_all_withdrawals.php
@@ -0,0 +1,18 @@
+<?php 
+
+session_start();
+try{
+$admin_id = empty($_SESSION["admin_id"]) ? "" : $_SESSION["admin_id"] ;
+if(!empty($admin_id)){
+$withdrawals = json_decode(file_get_contents("withdrawals.json"),true);
+echo json_encode(array("code"=> 0, "data"=>$withdrawals));
+}
+else{
+echo json_encode(array("code"=> 1, "data"=>[]));
+}
+}
+catch(Throwable $th){
+echo json_encode(array("code"=> 1, "data"=>[]));
+}
+
+?>
