d5367785a76c63ad7dbb7dc3068396d5b052393d
amethystcoder <75532154+amethystcoder@users.noreply.github.com>
2024-02-29 21:18:55 +0100

    Create get_user_pending_deposits.php

---
+++ serve/get_user_pending_deposits.php
@@ -0,0 +1,23 @@
+<?php 
+session_start();
+try{
+$user_id = empty($_SESSION["user_id"]) ? "" : $_SESSION["user_id"] ;
+if(!empty($user_id)){
+$deposits = json_decode(file_get_contents("deposits.json"),true);
+$user_deposits = [];
+for($i =0;$i<count($deposits);$i++){
+if($deposits[$i]["user_id"] == $user_id && $deposits[$i]["status"] == "pending"){
+$user_deposits[count($user_deposits)] = $deposits[$i];
+}
+}
+echo json_encode(array("code"=> 0, "data"=>$user_deposits));
+}
+else{
+echo json_encode(array("code"=> 2, "data"=>[]));
+}
+}
+catch(Throwable $th){
+echo json_encode(array("code"=> 1, "data"=>[]));
+}
+
+?>
