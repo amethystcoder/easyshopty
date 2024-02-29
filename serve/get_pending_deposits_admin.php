30a17a98fce2a0b52faa04156cde9dfaf745cead
amethystcoder <75532154+amethystcoder@users.noreply.github.com>
2024-02-29 21:17:56 +0100

    Create get_pending_deposits_admin.php

---
+++ serve/get_pending_deposits_admin.php
@@ -0,0 +1,24 @@
+<?php 
+
+session_start();
+try{
+$admin_id = empty($_SESSION["admin_id"]) ? "" : $_SESSION["admin_id"] ;
+if(!empty($admin_id)){
+$deposits = json_decode(file_get_contents("deposits.json"),true);
+$user_deposits = [];
+for($i =0;$i<count($deposits);$i++){
+if($deposits[$i]["status"] == "pending"){
+$user_deposits[count($user_deposits)] = $deposits[$i];
+}
+}
+echo json_encode(array("code"=> 0, "data"=>$deposits));
+}
+else{
+echo json_encode(array("code"=> 1, "data"=>[]));
+}
+}
+catch(Throwable $th){
+echo json_encode(array("code"=> 1, "data"=>[]));
+}
+
+?>
