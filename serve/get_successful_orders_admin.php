4c694917f3509a7a3d25e471f3bc6674a6a31ff0
amethystcoder <75532154+amethystcoder@users.noreply.github.com>
2024-02-29 21:23:01 +0100

    Create get_successful_orders_admin.php

---
+++ serve/get_successful_orders_admin.php
@@ -0,0 +1,24 @@
+<?php 
+
+session_start();
+try{
+$admin_id = empty($_SESSION["admin_id"]) ? "" : $_SESSION["admin_id"] ;
+if(!empty($admin_id)){
+$orders = json_decode(file_get_contents("orders.json"),true);
+$user_orders = [];
+for($i =0;$i<count($orders);$i++){
+if($orders[$i]["status"] == "successful"){
+$user_orders[count($user_orders)] = $orders[$i];
+}
+}
+echo json_encode(array("code"=> 0, "data"=>$orders));
+}
+else{
+echo json_encode(array("code"=> 1, "data"=>[]));
+}
+}
+catch(Throwable $th){
+echo json_encode(array("code"=> 1, "data"=>[]));
+}
+
+?>
