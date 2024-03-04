62f414c349036739b0a1dbbada6a47ab85bed6b3
amethystcoder <amethystguitaristj@gmail.com>
2024-03-04 21:40:23 +0000

    new changes

--- serve/make_deposit.php
+++ serve/make_deposit.php
@@ -1,28 +1,40 @@
 <?php
 
-`"price": price,
-"id": id,
-"type": type`;
-
-`
-setTimeout(function () {
-    if (next_url) {
-        var join = '?';
-        if (next_url.indexOf('?') > -1) {
-            join = '&'
-        }
-        var url = next_url + join + 'num=' + data.info.num + '&id=' + id + '&payer_name=' + payer_name + '&payer_bank=' + payer_bank + '&payer_cardno=' + payer_cardno + '&payer_name=' + payer_name + '&payer_mobile=' + payer_mobile + '&payer_upi=' + payer_upi + '&payer_email=' + payer_email + "&vip_id=" + vip_id + "&type=" + type2;
-        if (next_url.indexOf('paydragon') > -1 || next_url.indexOf('recharge2') > -1) {
-            window.location.href = url;
-        } else {
-            window.location.href = url;
-        }
-    } else {
-        window.location.href = '/index/ctrl/recharge2?num=' + data.info.num + '&type=' + type + "&vip_id=" + vip_id;
+function generate_deposit_id() {
+    $char_list = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C",
+                    "V","B","N","M","1","2","3","4","5","6","7","8","9"];
+    $result = "DEP";
+    for ($i=0; $i < 20; $i++) { 
+        $result .= $char_list[(time() * random_int(1111,9999)) % (count($char_list) - 1)];
     }
-},
-    1500);
-`;
+    return $result;
+}
+
+session_start();
+try {
+    $user_id = empty($_SESSION["user_id"]) ? "" : $_SESSION["user_id"] ;
+if(!empty($user_id)){
+    $price = $_POST["price"];
+    $deposit_id = generate_deposit_id();
+    $type = $_POST["type"];
+    $deposits = json_decode(file_get_contents("deposits.json"),true);
+    $new_deposit = array(
+        "price"=> $price,
+        "type"=> $type,
+        "deposit_id"=> $deposit_id,
+        "user_id" => $user_id,
+        "status" => "unpaid"
+    );
+    $deposits[count($deposits)] = $new_deposit;
+    $saved_successfully = file_put_contents("deposits.json",json_encode($deposits));
+    echo json_encode(array("code" => 0, "info"=> array("num" => $deposit_id)));
+}
+else{
+    echo json_encode(array("code" => 2, "info"=> "user unverified"));
+}
+} catch (\Throwable $th) {
+    echo json_encode(array("code" => 1, "info"=> "an error occured"));
+}
 
 
 ?>
