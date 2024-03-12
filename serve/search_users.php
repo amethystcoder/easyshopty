5368cbec10b659e308841d403e2c6afff83f5011
amethystcoder <amethystguitaristj@gmail.com>
2024-03-12 19:19:29 +0000

    new changes

--- serve/search_users.php
+++ serve/search_users.php
@@ -4,7 +4,6 @@ session_start();
 
 try {
     $admin_id = empty($_SESSION["admin_id"]) ? "" : $_SESSION["admin_id"];
-    echo json_encode($_GET);
     $addtime = $_GET["addtime"];
     $agent_id = $_GET["agent_id"];
     $agent_service_id = $_GET["agent_service_id"];
@@ -12,8 +11,11 @@ try {
     $level = $_GET["level"];
     $order = $_GET["order"];
     $selectList = $_GET["selectList"];
+    if($selectList == "-1"){ $selectList = "verified"; }
+    else { $selectList = "unverified"; }
     $tel = $_GET["tel"];
     $username = $_GET["username"];
+    $invite_code = $_GET["invite_code"];
     $users = json_decode(file_get_contents("users.json"),true);
     if (!empty($username)) {
         $filtered = [];
@@ -24,31 +26,56 @@ try {
         }
         $users = $filtered;
     }
-    if (condition) {
-        # code...
+    if (!empty($selectList)) {
+        $filtered = [];
+        for ($i=0; $i < count($users); $i++) { 
+            if($selectList == $users[$i]["is_real"]){
+                $filtered[count($filtered)] = $users[$i];
+            }
+        }
+        $users = $filtered;
     }
-    if (condition) {
-        # code...
+    if (!empty($tel)) {
+        $filtered = [];
+        for ($i=0; $i < count($users); $i++) { 
+            if($tel == $users[$i]["tel"]){
+                $filtered[count($filtered)] = $users[$i];
+            }
+        }
+        $users = $filtered;
     }
-    if (condition) {
-        # code...
+    if (!empty($level) && $level != "-1" ) {
+        $level = "VIP ".$level;
+        $filtered = [];
+        for ($i=0; $i < count($users); $i++) { 
+            if($level == $users[$i]["user_status"]){
+                $filtered[count($filtered)] = $users[$i];
+            }
+        }
+        $users = $filtered;
     }
-    if (condition) {
-        # code...
+    if (!empty($addtime)) {
+        $filtered = [];
+        for ($i=0; $i < count($users); $i++) { 
+            if($addtime == $users[$i]["date"]){
+                $filtered[count($filtered)] = $users[$i];
+            }
+        }
+        $users = $filtered;
     }
-    if (condition) {
-        # code...
+    if (!empty($invite_code)) {
+        $filtered = [];
+        for ($i=0; $i < count($users); $i++) { 
+            if($invite_code == $users[$i]["referral_code"]){
+                $filtered[count($filtered)] = $users[$i];
+            }
+        }
+        $users = $filtered;
     }
-    if (condition) {
-        # code...
-    }
-    if (condition) {
-        # code...
-    }
-    echo json_encode([]);
+    echo json_encode(array("code" => 0, "data"=>$users));
 
 } catch (\Throwable $th) {
-    //throw $th;
+    echo json_encode(array("code" => 1, "data"=>[]));
 }
 
 ?>
---
+++ serve/search_withdrawals.php
@@ -0,0 +1,54 @@
+<?php
+
+session_start();
+
+try {
+    $admin_id = empty($_SESSION["admin_id"]) ? "" : $_SESSION["admin_id"];
+    echo json_encode($_GET);
+    $addtime = $_GET["addtime"];
+    $agent_id = $_GET["agent_id"];
+    $agent_service_id = $_GET["agent_service_id"];
+    $group_id = $_GET["group_id"];
+    $level = $_GET["level"];
+    $order = $_GET["order"];
+    $selectList = $_GET["selectList"];
+    $tel = $_GET["tel"];
+    $username = $_GET["username"];
+    $users = json_decode(file_get_contents("users.json"),true);
+    if (!empty($username)) {
+        $filtered = [];
+        for ($i=0; $i < count($users); $i++) { 
+            if($username == $users[$i]["user_name"]){
+                $filtered[count($filtered)] = $users[$i];
+            }
+        }
+        $users = $filtered;
+    }
+    if (condition) {
+        # code...
+    }
+    if (condition) {
+        # code...
+    }
+    if (condition) {
+        # code...
+    }
+    if (condition) {
+        # code...
+    }
+    if (condition) {
+        # code...
+    }
+    if (condition) {
+        # code...
+    }
+    if (condition) {
+        # code...
+    }
+    echo json_encode([]);
+
+} catch (\Throwable $th) {
+    //throw $th;
+}
+
+?>
