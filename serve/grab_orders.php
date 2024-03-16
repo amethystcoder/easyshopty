ea287c7f9ff93437d41c01cfbb683314010fae75
amethystcoder <amethystguitaristj@gmail.com>
2024-03-16 08:53:22 +0000

    fixed new issue

--- serve/grab_orders.php
+++ serve/grab_orders.php
@@ -12,27 +12,43 @@ function create_oid() {
 }
 
 try {
+    $user_id = empty($_SESSION["user_id"]) ? "" : $SESSION["user_id"];
     $orders = json_decode(file_get_contents("orders.json"),true);
+    $users = json_decode(file_get_contents("users.json"),true);
+    $user = null;
+    $user_position = null;
     $incomplete_order_exists = false;
-    for ($i=0; $i < count($orders); $i++) { 
-        if ($orders[$i]["status"] == "pending") {
-            $incomplete_order_exists = true;
+    for ($i=0; $i < count($users); $i++) { 
+        if ($users[$i]["user_id"] == $user_id) {
+            $user = $users[$i];
+            $user_position = $i;
             break;
         }
     }
-    if ($incomplete_order_exists) {
-        echo json_encode(["code" => 1, "info"=>"please complete all pending orders before making a new order"]);
+    if ($user["balance"] < 5) {
+        echo json_encode(["code" => 1, "info"=>"you need to have minimum of 5TRX to make/complete an order"]);
     }
     else {
-        $selected_product = [];
-        $selected_product["code"] = 0;
-        $selected_product["oid"] = create_oid();
-        $selected_product["addtime"] = gmdate("M d Y H:i:s",time());
-        $selected_product["tymd"] = time();
-        $selected_product["status"] = "pending";
-        $orders[count($orders)] = $selected_product;
-        $saved = file_put_contents("orders.json",json_encode($orders));
-        echo json_encode($selected_product);
+        for ($i=0; $i < count($orders); $i++) { 
+            if ($orders[$i]["status"] == "pending") {
+                $incomplete_order_exists = true;
+                break;
+            }
+        }
+        if ($incomplete_order_exists) {
+            echo json_encode(["code" => 1, "info"=>"please complete all pending orders before making a new order"]);
+        }
+        else {
+            $selected_product = [];
+            $selected_product["code"] = 0;
+            $selected_product["oid"] = create_oid();
+            $selected_product["addtime"] = gmdate("M d Y H:i:s",time());
+            $selected_product["tymd"] = time();
+            $selected_product["status"] = "pending";
+            $orders[count($orders)] = $selected_product;
+            $saved = file_put_contents("orders.json",json_encode($orders));
+            echo json_encode($selected_product);
+        }
     }
 } catch (\Throwable $th) {
     echo json_encode(["code" => 2, "info"=>"an error occured"]);
