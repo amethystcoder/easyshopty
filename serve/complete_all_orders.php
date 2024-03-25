05b19430478eca919510297a02f9c3c9487dce3d
amethystcoder <75532154+amethystcoder@users.noreply.github.com>
2024-03-25 09:49:04 +0100

    Update complete_all_orders.php

--- serve/complete_all_orders.php
+++ serve/complete_all_orders.php
@@ -31,14 +31,14 @@ error_reporting(E_ALL ^ E_WARNING);
                         $new_earning_data = ["tymd"=>time(),"amount" => $earning, "date" => gmdate("M d Y H:i:s",time()),"user_id" => $user["user_id"]];
                         $user["balance"] += $earning;
                         $earnings[count($earnings)] = $new_earning_data;
-                    }
-                    for($j=0;$j < count($users);$j++){
+                        for($j=0;$j < count($users);$j++){
                         if($users[$j]["referral_code"] == $user["link_added_from"]){
                             $new_earning = $earning * (10/100);
                             $users[$j]["balance"] += $new_earning;
                             $new_earning_data = ["tymd"=>time(),"amount" => $new_earning, "date" => gmdate("M d Y H:i:s",time()), "user_id" => $users[$i]["user_id"]];
                             $earnings[count($earnings)] = $new_earning_data;
                         }
+                    }
                     }
                 }
             }
