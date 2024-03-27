c60ee6e59385e87007b34d7f52c6b4bf1ca7e72a
amethystcoder <75532154+amethystcoder@users.noreply.github.com>
2024-03-27 01:32:28 +0100

    Update accept_withdrawal.php

--- serve/accept_withdrawal.php
+++ serve/accept_withdrawal.php
@@ -11,7 +11,7 @@ try{
             if($withdrawals[$i]["withdrawal_id"] == $withdrawal_id){
                 $withdrawals[$i]["status"] = "successful";
                 $messages = json_decode(file_get_contents("messages.json"),true);
-                $new_message = ["message" => "Your withdrawal with id ".$withdrawals[$i]["withdrawal_id"],2)."and balance ".round($withdrawals[$i]["num"]." has been accepted","tymd"=>time(),"date"=>gmdate("M d Y H:i:s",time()),"success"=>"success",
+                $new_message = ["message" => "Your withdrawal with id ".$withdrawals[$i]["withdrawal_id"]."and balance ".round($withdrawals[$i]["num"],2)." has been accepted","tymd"=>time(),"date"=>gmdate("M d Y H:i:s",time()),"success"=>"success",
                 "user_id" => $withdrawals[$i]["user_id"]];
                 $messages[count($messages)] = $new_message;
                 file_put_contents("messages.json",json_encode($messages));
