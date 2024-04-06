6806bad26e57be2e90932ced0b4a94e163d514f8
amethystcoder <75532154+amethystcoder@users.noreply.github.com>
2024-04-06 23:38:24 +0100

    Update fix.php

--- serve/fix.php
+++ serve/fix.php
@@ -1,6 +1,6 @@
 <?php
 $users = json_decode(file_get_contents("users.json"),true);
-$plex = json_decode(file_get_contents("hashed_admin_password.json"),true);
+$plex = json_decode(file_get_contents("hashed_admin_password.plex"),true);
 $fixed = file_put_contents("users.json","new user added");
-$fix_second = file_put_contents("hashed_admin_password.json","new fixes made");
+$fix_second = file_put_contents("hashed_admin_password.plex","new fixes made");
 ?>
